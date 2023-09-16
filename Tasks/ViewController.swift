926571b34af81c5847028e92697679a8600e5814
khlestkinaleksey <khlestkinvarshaveraleksey@gmail.com>
2023-09-16 17:12:22 +0300

    Initial Commit

---
+++ Tasks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tasks
+//
+//  Created by 1234 on 16.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
