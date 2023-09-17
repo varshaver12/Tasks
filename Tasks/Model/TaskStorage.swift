e2e65ddfd5d0f6bfc08cb5a4d90cba8f849f73d3
khlestkinaleksey <khlestkinvarshaveraleksey@gmail.com>
2023-09-17 18:30:17 +0300

    Added function for configure cell getConfigureTaskCellConstraints()

--- Tasks/Model/TaskStorage.swift
+++ Tasks/Model/TaskStorage.swift
@@ -11,7 +11,7 @@ class TaskStorage: TaskStorageProtocol {
     func loadTasks() -> [TaskProtocol] {
         let testTasks = [
             Task(title: "Купить макбук", type: .normal, status: .completed),
-            Task(title: "Покаормить сгущенку", type: .important, status: .planned),
+            Task(title: "Покормить сгущенку", type: .important, status: .planned),
             Task(title: "Заказать еду", type: .normal, status: .planned),
             Task(title: "Сделать ремонт на кухне", type: .normal, status: .planned),
             Task(title: "Попить воды", type: .important, status: .completed)]
--- Tasks/TaskListController.swift
+++ Tasks/TaskListController.swift
@@ -8,82 +8,90 @@
 import UIKit
 
 class TaskListController: UITableViewController {
+    
+    let taskStorage: TaskStorageProtocol = TaskStorage()
+    var tasks: [TaskPriority:[TaskProtocol]] = [:]
+    var sectionsTypesPosition: [TaskPriority] = [.important, .normal]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        loadTasks()
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        
+        return tasks.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        let taskType = sectionsTypesPosition[section]
+        guard let currentTaskType = tasks[taskType] else {
+            return 0
+        }
+        return currentTaskType.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return getConfigureTaskCellConstraints(for: indexPath)
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+    private func loadTasks() {
+        sectionsTypesPosition.forEach { taskType in
+            tasks[taskType] = []
+        }
+        taskStorage.loadTasks().forEach { task in
+            tasks[task.type]?.append(task)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let taskType = sectionsTypesPosition[section]
+        switch taskType {
+        case .important:
+            return "Важные"
+        case .normal:
+            return "Текущие"
+        }
+    }
+    
+    // MARK: Private functions
+    
+    private func getConfigureTaskCellConstraints(for indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "taskCellConstraints", for: indexPath)
+        let taskType = sectionsTypesPosition[indexPath.section]
+        guard let currentTask = tasks[taskType]?[indexPath.row] else {
+            return cell
+        }
+        
+        let symbolLabel = cell.viewWithTag(1) as? UILabel
+        let textLabel = cell.viewWithTag(2) as? UILabel
+        
+        symbolLabel?.text = getSymbolForTask(with: currentTask.status)
+        textLabel?.text = currentTask.title
+        
+        if currentTask.status == .planned {
+            textLabel?.textColor = .black
+            symbolLabel?.textColor = .black
+        } else {
+            textLabel?.textColor = .lightGray
+            symbolLabel?.textColor = .lightGray
+        }
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    private func getSymbolForTask(with status: TaskStatus) -> String {
+        switch status {
+        case .planned:
+            return "\u{25CB}"
+        case .completed:
+            return "\u{25C9}"
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
